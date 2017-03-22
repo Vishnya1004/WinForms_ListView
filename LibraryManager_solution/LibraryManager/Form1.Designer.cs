bfdc617bc5def184f215d1d3523384898f71f1cb
Vishnya1004 <marina.vishnevska@gmail.com>
2017-03-22 18:20:19 +0200

    Added Load Method, Save - not finished

--- LibraryManager_solution/LibraryManager/Form1.Designer.cs
+++ LibraryManager_solution/LibraryManager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LibraryManager
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "01",
+            "Joanne",
+            "Rowling"}, -1);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editAndSaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CatalogTable = new System.Windows.Forms.ListView();
+            this.Id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Author_FName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Autor_LName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1468, 49);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(75, 45);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(327, 46);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(327, 46);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(327, 46);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem1,
+            this.editAndSaveToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(80, 45);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // editToolStripMenuItem1
+            // 
+            this.editToolStripMenuItem1.Name = "editToolStripMenuItem1";
+            this.editToolStripMenuItem1.Size = new System.Drawing.Size(327, 46);
+            this.editToolStripMenuItem1.Text = "Edit";
+            // 
+            // editAndSaveToolStripMenuItem
+            // 
+            this.editAndSaveToolStripMenuItem.Name = "editAndSaveToolStripMenuItem";
+            this.editAndSaveToolStripMenuItem.Size = new System.Drawing.Size(327, 46);
+            this.editAndSaveToolStripMenuItem.Text = "Edit and save";
+            // 
+            // CatalogTable
+            // 
+            this.CatalogTable.AutoArrange = false;
+            this.CatalogTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Id,
+            this.Author_FName,
+            this.Autor_LName,
+            this.Title});
+            this.CatalogTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CatalogTable.GridLines = true;
+            this.CatalogTable.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.CatalogTable.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem2});
+            this.CatalogTable.Location = new System.Drawing.Point(0, 49);
+            this.CatalogTable.Name = "CatalogTable";
+            this.CatalogTable.Size = new System.Drawing.Size(1468, 1363);
+            this.CatalogTable.TabIndex = 1;
+            this.CatalogTable.UseCompatibleStateImageBehavior = false;
+            this.CatalogTable.View = System.Windows.Forms.View.Details;
+            this.CatalogTable.SelectedIndexChanged += new System.EventHandler(this.CatalogTable_SelectedIndexChanged);
+            // 
+            // Id
+            // 
+            this.Id.Text = "Id";
+            this.Id.Width = 40;
+            // 
+            // Author_FName
+            // 
+            this.Author_FName.Text = "Author_FName";
+            this.Author_FName.Width = 357;
+            // 
+            // Autor_LName
+            // 
+            this.Autor_LName.Text = "Autor_LName";
+            this.Autor_LName.Width = 323;
+            // 
+            // Title
+            // 
+            this.Title.Text = "Title";
+            this.Title.Width = 200;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1468, 1412);
+            this.Controls.Add(this.CatalogTable);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Library";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem editAndSaveToolStripMenuItem;
+        private System.Windows.Forms.ListView CatalogTable;
+        private System.Windows.Forms.ColumnHeader Id;
+        private System.Windows.Forms.ColumnHeader Author_FName;
+        private System.Windows.Forms.ColumnHeader Autor_LName;
+        private System.Windows.Forms.ColumnHeader Title;
     }
 }
 
--- LibraryManager_solution/LibraryManager/Form1.cs
+++ LibraryManager_solution/LibraryManager/Form1.cs
@@ -7,15 +7,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace LibraryManager
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        XmlDocument xmlDoc;
+        private XmlElement xmlRoot;
+        private string Path { get; }
+
+        public MainWindow()
         {
             InitializeComponent();
-            //test
+            Path = @"../../Repository.xml";
+            LoadXmlDoc();
+            SaveToXmlDoc();
+
+        }
+
+        private void LoadXmlDoc()
+        {
+            xmlDoc = new XmlDocument();
+            xmlDoc.Load(Path);
+            xmlRoot = xmlDoc.DocumentElement;
+            //foreach (XmlNode xmlNode in xmlRoot)
+            //{
+            //xmlNode.ChildNodes
+            ListViewItem item = null;
+            foreach (XmlNode xmlChildNode in xmlRoot.FirstChild.ChildNodes)
+            {
+                if (xmlChildNode.Name == "id")
+                {
+                    item = new ListViewItem(xmlChildNode.InnerText);
+                    CatalogTable.Items.Add(item);
+                }
+                else if (xmlChildNode.Name == "autorFName")
+                {
+                    item.SubItems.Add(xmlChildNode.InnerText);
+                }
+                else if (xmlChildNode.Name == "autorLName")
+                {
+                    item.SubItems.Add(xmlChildNode.InnerText);
+                }
+                else if (xmlChildNode.Name == "title")
+                {
+                    item.SubItems.Add(xmlChildNode.InnerText);
+                }
+            }
+            //}
+        }
+
+        private void SaveToXmlDoc()
+        {
+            xmlDoc = new XmlDocument();
+            xmlDoc.Load(Path);
+            xmlRoot = xmlDoc.DocumentElement;
+            XmlElement bookXmlElement = xmlDoc.CreateElement("book");
+            XmlElement idXmlElement = xmlDoc.CreateElement("id");
+            XmlElement fNameXmlElement = xmlDoc.CreateElement("FName");
+            XmlElement lNameXmlElement = xmlDoc.CreateElement("LNAme");
+            XmlElement titleXmlElement = xmlDoc.CreateElement("title");
+            XmlText idXmlText = null;
+            XmlText fNameXmlText = null;
+            XmlText lNameXmlText = null;
+            XmlText titleXmlText = null;
+
+
+            foreach (ListViewItem item in CatalogTable.Items)
+            {
+                int i = 0;
+                bookXmlElement.AppendChild(idXmlElement);
+                idXmlText = xmlDoc.CreateTextNode(item.Text);
+                bookXmlElement.AppendChild(fNameXmlElement);
+                fNameXmlText = xmlDoc.CreateTextNode(item.SubItems[i++].Text);
+                bookXmlElement.AppendChild(lNameXmlElement);
+                lNameXmlText = xmlDoc.CreateTextNode(item.SubItems[i++].Text);
+                bookXmlElement.AppendChild(titleXmlElement);
+                titleXmlText = xmlDoc.CreateTextNode(item.SubItems[i++].Text);
+                xmlRoot.AppendChild(bookXmlElement);
+
+            }
+            xmlDoc.Save(Path);
+
+
+        }
+
+        private void CatalogTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
